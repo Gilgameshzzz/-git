1dc1618bb753c87716df8aee500e5e1d1b02736a
Gilgamesh <619959856@qq.com>
2018-09-29 18:49:52 +0800

    first try
    还要些细节没有整理

---
+++ django_cart/users/urls.py
@@ -0,0 +1,12 @@
+# Filename  : urls.py
+# Date  : 2018/9/26
+from django.conf.urls import url
+
+from users import views
+
+urlpatterns = [
+	url(r'^index/', views.index, name='index'),
+	url(r'^login/', views.login, name='login'),
+	url(r'^register/', views.register, name='register'),
+	url(r'^base/', views.base, name='base'),
+]
